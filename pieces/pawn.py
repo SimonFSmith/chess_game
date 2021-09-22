b7fd07a2f9df10b65ba156247769e8dd3c06affa
1935114 <felix65658@hotmail.com>
2021-09-22 14:23:52 -0400

    Snake case pawn.py

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -10,32 +10,32 @@ class Pawn(Piece):
     def __init__(self, x, y, type=True):
         super(Pawn, self).__init__(type)
         if self.white:
-            self.pieceimage = resources.spritesheet[WHITE_PAWN]
+            self.piece_image = resources.sprite_sheet[WHITE_PAWN]
         else:
-            self.pieceimage = resources.spritesheet[BLACK_PAWN]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
+            self.piece_image = resources.sprite_sheet[BLACK_PAWN]
+        self.piece_sprite = pyglet.sprite.Sprite(self.piece_image, x * 75, y * 75)
 
     # Calculate possible moves
-    def GetThreatSquares(self, board):
-        x = self.piecesprite.x // 75
-        y = self.piecesprite.y // 75
-        ListOfMoves = []
+    def get_threat_squares(self, board):
+        x = self.piece_sprite.x // 75
+        y = self.piece_sprite.y // 75
+        list_of_moves = []
         if self.white and y < 7:
             if board[y + 1][x] is None:
-                ListOfMoves.append((y + 1, x))
+                list_of_moves.append((y + 1, x))
                 if y == 1 and board[y + 2][x] is None:
-                    ListOfMoves.append((y + 2, x))
+                    list_of_moves.append((y + 2, x))
             if x < 7 and board[y + 1][x + 1] is not None and not board[y + 1][x + 1].white:
-                ListOfMoves.append((y + 1, x + 1))
+                list_of_moves.append((y + 1, x + 1))
             if x > 0 and board[y + 1][x - 1] is not None and not board[y + 1][x - 1].white:
-                ListOfMoves.append((y + 1, x - 1))
+                list_of_moves.append((y + 1, x - 1))
         elif not self.white and y > 0:
             if board[y - 1][x] is None:
-                ListOfMoves.append((y - 1, x))
+                list_of_moves.append((y - 1, x))
                 if y == 6 and board[y - 2][x] is None:
-                    ListOfMoves.append((y - 2, x))
+                    list_of_moves.append((y - 2, x))
             if x < 7 and board[y - 1][x + 1] is not None and board[y - 1][x + 1].white:
-                ListOfMoves.append((y - 1, x + 1))
+                list_of_moves.append((y - 1, x + 1))
             if x > 0 and board[y - 1][x - 1] is not None and board[y - 1][x - 1].white:
-                ListOfMoves.append((y - 1, x - 1))
-        return ListOfMoves
+                list_of_moves.append((y - 1, x - 1))
+        return list_of_moves
