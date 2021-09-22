77ae579e6da4ff2b87438c33d2e04d30e8588ac8
1935114 <felix65658@hotmail.com>
2021-09-22 14:30:07 -0400

    Snake case rook.py

--- pieces/rook.py
+++ pieces/rook.py
@@ -8,47 +8,47 @@ class Rook(Piece):
     def __init__(self, x, y, type=True):
         super(Rook, self).__init__(type)
         if self.white:
-            self.pieceimage = resources.spritesheet[WHITE_ROOK]
+            self.piece_image = resources.sprite_sheet[WHITE_ROOK]
         else:
-            self.pieceimage = resources.spritesheet[BLACK_ROOK]
-        self.piecesprite = pyglet.sprite.Sprite(self.pieceimage, x * 75, y * 75)
+            self.piece_image = resources.sprite_sheet[BLACK_ROOK]
+        self.piece_sprite = pyglet.sprite.Sprite(self.piece_image, x * 75, y * 75)
         self.moved = False
 
-    def ChangeLocation(self, x, y, board):
-        self.piecesprite.x = x * 75
-        self.piecesprite.y = y * 75
+    def change_location(self, x, y, board):
+        self.piece_sprite.x = x * 75
+        self.piece_sprite.y = y * 75
         self.moved = True
 
-    def GetThreatSquares(self, board):
-        x = self.piecesprite.x // 75
-        y = self.piecesprite.y // 75
-        ListOfMoves = []
+    def get_threat_squares(self, board):
+        x = self.piece_sprite.x // 75
+        y = self.piece_sprite.y // 75
+        list_of_moves = []
         if y > 0:
             for i in range(y - 1, -1, -1):
                 if board[i][x] is not None:
                     if board[i][x].white != self.white:
-                        ListOfMoves.append((i, x))
+                        list_of_moves.append((i, x))
                     break
-                ListOfMoves.append((i, x))
+                list_of_moves.append((i, x))
         if y < 7:
             for i in range(y + 1, 8):
                 if board[i][x] is not None:
                     if board[i][x].white != self.white:
-                        ListOfMoves.append((i, x))
+                        list_of_moves.append((i, x))
                     break
-                ListOfMoves.append((i, x))
+                list_of_moves.append((i, x))
         if x > 0:
             for i in range(x - 1, -1, -1):
                 if board[y][i] is not None:
                     if board[y][i].white != self.white:
-                        ListOfMoves.append((y, i))
+                        list_of_moves.append((y, i))
                     break
-                ListOfMoves.append((y, i))
+                list_of_moves.append((y, i))
         if x < 7:
             for i in range(x + 1, 8):
                 if board[y][i] is not None:
                     if board[y][i].white != self.white:
-                        ListOfMoves.append((y, i))
+                        list_of_moves.append((y, i))
                     break
-                ListOfMoves.append((y, i))
-        return ListOfMoves
+                list_of_moves.append((y, i))
+        return list_of_moves
