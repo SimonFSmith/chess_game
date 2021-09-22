9c37cef9c0fdbb62808a1b19ac0dc198a3823cb0
1935114 <felix65658@hotmail.com>
2021-09-22 12:57:36 -0400

    Snake case main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from guiChess import Chess, pyglet
 
 
 def main():
-    mygame = Chess()
-    pyglet.clock.schedule_interval(mygame.update, 1 / 60.)
+    my_game = Chess()
+    pyglet.clock.schedule_interval(my_game.update, 1 / 60.)
     pyglet.app.run()
 
 
